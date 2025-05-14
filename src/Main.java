454b9e22e056e701cd424852ea1a64034d20d8ee
Kevin Ernest Long <kevinelong@gmail.com>
2025-05-14 10:56:03 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+interface MyInterface {
+    default void doSomething(){
+        System.out.println("Default doing something");
+    }
+}
+
+class ClassA implements MyInterface {
+    @Override
+    public void doSomething() {
+        System.out.println("Class A doing something");
+    }
+}
+
+class ClassB implements MyInterface{
+    public void doSomethingElse() {
+        System.out.println("Class B doing something else");
+    }
+}
+class ClassC {
+    public void doSomethingElse() {
+        System.out.println("Class B doing something else");
+    }
+}
 
 public class Main {
+    public static void main(String[] args) {
+        Object[] objects = {new ClassA(), new ClassB(), new ClassC()};
+
+        for (Object obj : objects) {
+            if (obj instanceof MyInterface) {
+                ((MyInterface) obj).doSomething();
+//                MyInterface myInterface = (MyInterface) obj;
+//                myInterface.doSomething();
+            } else {
+                System.out.println("Object does not implement MyInterface");
+            }
+        }
+    }
 }
